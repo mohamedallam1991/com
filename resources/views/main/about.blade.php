c1333a1ca0a99d9708fee271d2c101164dce73ef
Bobby Walker <bobjwalker@icloud.com>
2017-09-01 18:34:02 -0500

    Added about layout

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.layout')
 
 @section('content')
-    <h1>About Us</h1>
+    <div class="about">
+        <div class="img-holder">
+            <img src="{{url('/images/RevolutionLogos.svg')}}" alt="Revolution Logo">
+        </div>
+
+        <h1>About Us</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut accumsan turpis, fermentum dictum sapien. Vestibulum sodales mi vitae tortor ultricies, non egestas metus fermentum. Sed sodales dictum eros, eu consectetur justo porttitor ut. In quis quam sodales, malesuada magna nec, aliquam arcu. Curabitur non efficitur ipsum. Sed imperdiet tellus ac mauris pellentesque, et ultricies felis pretium. Curabitur eget nunc sagittis odio pellentesque suscipit ut eu metus.
+        </p><p>
+            Sed sed cursus lorem. Donec tristique consequat nibh, et pellentesque nunc blandit quis. Donec id tortor faucibus, varius neque vel, ullamcorper lectus. Fusce at sodales ex. Sed feugiat tempus dolor vitae efficitur. Vestibulum dignissim pulvinar libero sit amet sollicitudin. Curabitur suscipit metus nec diam tincidunt fermentum euismod ut erat. Donec nibh velit, interdum eu laoreet ut, volutpat quis leo. Quisque et vehicula nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </p>
+    </div>
+
 @endsection
