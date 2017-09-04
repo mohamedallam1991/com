57077e2374240ef69b75fbe50dff1d169dfd6a9b
Bobby Walker <bobjwalker@icloud.com>
2017-09-04 17:51:39 -0500

    Admin login layout

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,6 +1,29 @@
 @extends('layout.layout')
 
-@section('content')
-    <h1>Admin Login</h1>
+@section('styles')
+    <link href="{{ url('css/admin.css') }}" rel="stylesheet">
+@endsection
 
+@section('content')
+
+    <div class="common">
+        <h1>Admin Login</h1>
+        <form method="post">
+            <div class="form-group">
+                <label id="emailLabel" for="email" class="control-label label-hidden">Email Address</label>
+                <input type="text" class="form-control" id="email" placeholder="Email Address"/>
+            </div>
+            <div class="form-group">
+                <label id="passwordLabel" for="password" class="control-label label-hidden">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password"/>
+            </div>
+            <button type="submit" class="btn btn-default btn-block" id="login">Login</button>
+        </form>
+    </div>
+@endsection
+
+@section('script.js')
+    @parent
+    <script src="{{ url('js/constrain.js') }}"></script>
+    <script src="{{ asset('js/main.contact.js') }}"></script>
 @endsection
