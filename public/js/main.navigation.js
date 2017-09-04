8e6904badfd0c3402e999a67560de9d7801f7137
Bobby Walker <bobjwalker@icloud.com>
2017-09-04 17:46:20 -0500

    Fixed javascript bug in navbar active state

--- public/js/main.navigation.js
+++ public/js/main.navigation.js
@@ -1,8 +1,11 @@
 $(function() {
    var alist = $('#navbar a');
    var path = window.location.pathname;
+   productsPath = path.indexOf('products/cat/');
    alist.each(function(a) {
        if(alist[a].getAttribute('href') === path) {
+           $(alist[a].parentNode).addClass('active');
+       } else if(productsPath > 0 && alist[a].getAttribute('href').indexOf('products') > 0) {
            $(alist[a].parentNode).addClass('active');
        } else {
            $(alist[a].parentNode).removeClass('active');
