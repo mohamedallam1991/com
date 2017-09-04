403e0661b9e2883b20dc4ee7f3fb95ebfa045e3e
Bobby Walker <bobjwalker@icloud.com>
2017-09-04 18:18:27 -0500

    Basic application view, no details yet

--- resources/views/admin/application.blade.php
+++ resources/views/admin/application.blade.php
@@ -1,6 +1,21 @@
 @extends('layout.layout')
 
-@section('content')
-    <h1>Application View lives here...</h1>
 
-    @endsection
+@section('styles')
+    <link rel="stylesheet" href="{{ url('css/admin.css')  }}">
+@endsection
+
+@section('content')
+    @include('admin.stubs.navigation')
+    <div class="col-xs-12 col-md-10 common">
+        <h1>Dealer Application</h1>
+        <p>List out the application details when provided by client</p>
+    </div>
+@endsection
+
+@section('script.js')
+    @parent
+    <script src="{{ url('js/admin.navigation.js') }}"></script>
+    <script src="{{ url('js/admin.tables.js') }}"></script>
+    <script src="{{ url('js/constrain.js') }}"></script>
+@endsection
