eabb0d073fae680408128432a4c926b2ea0c1578
Bobby Walker <bobjwalker@icloud.com>
2017-09-06 12:28:07 -0500

    Base tracking page, not sure if it's really needed...

--- resources/views/admin/tracking.blade.php
+++ resources/views/admin/tracking.blade.php
@@ -1,5 +1,20 @@
 @extends('layout.layout')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ url('css/admin.css')  }}">
+@endsection
+
 @section('content')
-    <h1>Add Order Tracking lives here...</h1>
+    @include('admin.stubs.navigation')
+    <div class="col-xs-12 col-md-10 common">
+        <h1>Add Order Tracking lives here...</h1>
+    </div>
+@endsection
+
+@section('script.js')
+    @parent
+    <script src="{{ url('js/admin.navigation.js') }}"></script>
+    <script src="{{ url('js/admin.tables.js') }}"></script>
+    <script src="{{ url('js/admin.products.js') }}"></script>
+    <script src="{{ url('js/constrain.js') }}"></script>
 @endsection
