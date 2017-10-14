6f61908feea885d9a7b9537163d9970685f61123
= <=>
2017-10-14 13:54:26 +0100

    backend

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\User::class, function (Faker $faker) {
+    static $password;
+    return [
+        'name' => $faker->name,
+        'image' => $faker->imageUrl($width = 640, $height = 480)
+    ];
+});
